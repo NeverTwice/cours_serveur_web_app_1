c18898c9db084f611afa9955b8a581173bd112de
sam <samuelantunes@hotmail.fr>
2019-09-20 13:47:41 +0200

    Docker-compose set-up

--- conf.inc.php
+++ conf.inc.php
@@ -1,9 +1,9 @@
 <?php
 
 //Constante pour la BDD
-define("HOST", "mysql:host=localhost;dbname=esgi1i;charset=utf8");
-define("USERBDD", "root");
-define("MDPBDD", "");
+define("HOST", "mysql:host=mysql;dbname=esgi1i;charset=utf8");
+define("USERBDD", "docker");
+define("MDPBDD", "docker");
 
 //Constante pour la sécurité
 define("GRAIN_DE_SABLE", "the_ravers");
